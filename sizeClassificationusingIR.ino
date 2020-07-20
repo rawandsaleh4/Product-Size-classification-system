6cbb2a47e63cf9872afb260f601a6034b33265ec
Rawand Saleh <45201070+rawandsaleh4@users.noreply.github.com>
2020-07-20 22:25:03 +0300

    Add files via upload

---
+++ sizeClassificationusingIR.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <Servo.h>
+Servo myservo;
+#define LSA 90 // large size angle
+#define MSA 0 // meduim size angle
+#define SSA -90 // small size angle
+#define IR1 2 // Sensor1
+#define IR2 3 //Sensor2
+#define IR3 4 //Sensor3
+void setup() {
+  // put your setup code here, to run once:
+ myservo.attach(5); 
+ for(int i=2;i<=4;i++){
+ pinMode(i,INPUT); // infrared light sensors
+}
+myservo.write(0);}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(digitalRead(IR1)==1&&digitalRead(IR2)==0&&digitalRead(IR3)==0){
+  myservo.write(SSA);
+  delay(15);
+}
+if(digitalRead(IR1)==1&&digitalRead(IR2)==1&&digitalRead(IR3)==0){
+  myservo.write(MSA);
+  delay(15);
+}
+if(digitalRead(IR1)==1&&digitalRead(IR2)==1&&digitalRead(IR3)==1){
+  myservo.write(LSA);
+  delay(15);
+}
+
+}
